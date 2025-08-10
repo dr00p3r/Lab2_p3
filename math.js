19c6e8f76132297cb030b3755fd6fe5dc5ae877c
dr00p3r <joancobe64@gmail.com>
2025-08-10 12:46:43 -0500

    Funciones matemáticas de comprobación de primos y maximo comun divisor

---
+++ math.js
@@ -0,0 +1,22 @@
+function maximoComunDivisor(int1, int2) {
+    while (int2) {
+        const temp = int2;
+        int2 = int1 % int2;
+        int1 = temp;
+    }
+    return int1;
+}
+
+function comprobarPrimo(int){
+    if (int <= 1) return true;
+
+    for(let i = 2; i*i<=int; i++){
+        if(int % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = {maximoComunDivisor, comprobarPrimo}
+
---
+++ math.test.js
@@ -0,0 +1,23 @@
+const { maximoComunDivisor, comprobarPrimo } = require('./math');
+
+describe('Pruebas de la funcion para encontrar el maximo comun divisor entre dos numeros', () => { 
+        test('El maximo comun divisor entre 48 y 18 debe dar 6', () => {
+            expect(maximoComunDivisor(48, 18)).toBe(6);
+        });
+        
+        test('El maximo comun divisor 10 y 6 debe dar 2', () => {
+            expect(maximoComunDivisor(10, 6)).toBe(2);
+        });
+    }
+);
+
+describe('Pruebas de la funcion para comprobar si un numero es primo', () => { 
+        test('La comprobación de primo de 4 debe ser falso', () => {
+            expect(comprobarPrimo(4).toBe(false));
+        });
+        
+        test('La comprobación de primo de 17 debe ser verdadero', () => {
+            expect(comprobarPrimo(17).toBe(true));
+        });
+    }
+);
